9e57c2eee67e2304c73459095a2ae8ef25c07361
= <ds@dansim.co>
2024-10-17 14:04:48 -0500

    css based solution

---
+++ src/segment-control.js
@@ -0,0 +1,15 @@
+// turns out this is probably best done with radio buttons and css
+
+// class SegmentControl extends HTMLElement {
+
+//   selectValue() {
+
+//   }
+
+//   connectedCallback() {
+
+//   }
+
+// }
+
+// customElements.define("segment-control", SegmentControl);
